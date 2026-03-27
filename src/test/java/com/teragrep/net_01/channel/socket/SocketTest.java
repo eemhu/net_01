a568e33d53f6130a178e555b2e47866474224a2d
eemhu <125959687+eemhu@users.noreply.github.com>
2026-03-27 15:51:02 +0200

    add SocketTest read/write test

--- src/test/java/com/teragrep/net_01/channel/socket/SocketTest.java
+++ src/test/java/com/teragrep/net_01/channel/socket/SocketTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.charset.StandardCharsets;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public final class SocketTest {
@@ -27,5 +32,89 @@ public final class SocketTest {
             socketCh.close();
             socket.close();
         });
+    }
+
+    @Test
+    void testPlainSocketWrite() {
+        Assertions.assertDoesNotThrow(() -> {
+            // Create ServerSocketChannel and bind it to port=9090
+            ServerSocketChannel socketCh = ServerSocketChannel.open();
+            socketCh.bind(new InetSocketAddress(9090));
+
+            // Init client
+            java.net.Socket clientSocket = new java.net.Socket("localhost", 9090);
+            final BufferedReader in = new BufferedReader(new InputStreamReader(Assertions.assertDoesNotThrow(clientSocket::getInputStream)));
+
+            // Init PlainSocket
+            Socket socket = new PlainSocket(socketCh.accept());
+
+            socket.write(stringToBuffer("helloWorld\n"));
+
+            final String readLine = in.readLine();
+
+            Assertions.assertEquals("helloWorld", readLine);
+
+            clientSocket.close();
+            socketCh.close();
+            socket.close();
+        });
+    }
+
+    @Test
+    void testPlainSocketRead() {
+        Assertions.assertDoesNotThrow(() -> {
+            // Create ServerSocketChannel and bind it to port=9090
+            ServerSocketChannel socketCh = ServerSocketChannel.open();
+            socketCh.bind(new InetSocketAddress(9090));
+
+            // Init client
+            java.net.Socket clientSocket = new java.net.Socket("localhost", 9090);
+            final PrintWriter out = new PrintWriter(Assertions.assertDoesNotThrow(clientSocket::getOutputStream), true);
+
+            // Init PlainSocket
+            Socket socket = new PlainSocket(socketCh.accept());
+
+            out.println("worldHello");
+
+            ByteBuffer[] bufs = emptyBuffers(1, 10);
+            socket.read(bufs);
+
+            Assertions.assertEquals("worldHello", bufferToString(bufs));
+
+            clientSocket.close();
+            socketCh.close();
+            socket.close();
+        });
+    }
+
+    private String bufferToString(final ByteBuffer[] bufs) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (final ByteBuffer buf : bufs) {
+            buf.flip();
+            while (buf.hasRemaining()) {
+                stringBuilder.append((char) buf.get());
+            }
+            buf.flip();
+        }
+        return stringBuilder.toString();
+    }
+
+    private ByteBuffer[] stringToBuffer(final String str) {
+        final ByteBuffer[] arr = new ByteBuffer[1];
+
+        final byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        arr[0] = ByteBuffer.wrap(bytes);
+
+        return arr;
+    }
+
+    private ByteBuffer[] emptyBuffers(int n, int bytesEach) {
+        final ByteBuffer[] arr = new ByteBuffer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = ByteBuffer.allocate(bytesEach);
+        }
+
+        return arr;
     }
 }
