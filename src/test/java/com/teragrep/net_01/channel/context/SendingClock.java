40b56f8a5672f3b47317118a75e1e67e73cab508
eemhu <125959687+eemhu@users.noreply.github.com>
2026-04-07 13:57:56 +0300

    SendingClock cleanup

--- src/test/java/com/teragrep/net_01/channel/context/SendingClock.java
+++ src/test/java/com/teragrep/net_01/channel/context/SendingClock.java
@@ -1,8 +1,9 @@
 package com.teragrep.net_01.channel.context;
 
 import com.teragrep.buf_01.buffer.pool.OpeningPool;
-import com.teragrep.net_01.channel.LeaseToString;
+import com.teragrep.net_01.channel.StringToLease;
 import com.teragrep.net_01.channel.buffer.TrackedMemorySegmentLease;
+import com.teragrep.net_01.channel.buffer.writable.Writeable;
 
 import java.util.function.Consumer;
 
@@ -21,12 +22,14 @@ public final class SendingClock implements Clock {
     public void advance(final TrackedMemorySegmentLease bufferLease) {
         final StringBuilder stringBuilder = new StringBuilder();
         while (bufferLease.hasNext()) {
-            stringBuilder.append((char)bufferLease.next().byteValue());
+            final char c = (char) bufferLease.next().byteValue();
+            stringBuilder.append(c);
         }
 
         final String str = stringBuilder.toString();
 
-        ctx.egress().accept(new LeaseToString(str, pool).toWriteable());
+        final Writeable w = new StringToLease(str, pool).toWriteable();
+        ctx.egress().accept(w);
 
         consumer.accept(str);
     }
